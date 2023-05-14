d03e9a2145175b076a099bda5561e1522580a346
Solo-steven <ssteven19@kimo.com>
2023-05-14 15:10:30 +0800

    feat: implement basic web backend api

--- packages/rust_compiler/src/lib.rs
+++ packages/rust_compiler/src/lib.rs
@@ -13,12 +13,12 @@ use codegen::Codegen;
 
 #[derive(Debug, PartialEq)]
 pub struct ExecuteResult {
-    llvm_code: Option<String>,
-    return_value: f64
+    pub llvm_code: Option<String>,
+    pub return_value: f64
 }
 
-pub fn execute_program(code_string: &'static str, emit_llvm: bool) -> ExecuteResult{
-    let mut parser = Parser::new(String::from(code_string));
+pub fn execute_program(code_string: String, emit_llvm: bool) -> ExecuteResult{
+    let mut parser = Parser::new(code_string);
     let program_ast = parser.parse();
     let llvm_context = Context::create();
     let mut code_generator = Codegen::new(&llvm_context, &program_ast);
@@ -28,24 +28,12 @@ pub fn execute_program(code_string: &'static str, emit_llvm: bool) -> ExecuteRes
 
     if emit_llvm {
         return  ExecuteResult{
-            llvm_code: None,
+            llvm_code: Some(llvm_code_string),
             return_value: execute_return_value,
         }
     }
     return ExecuteResult{
-        llvm_code: Some(llvm_code_string),
+        llvm_code: None,
         return_value: execute_return_value,
     }
-}
-
-fn main() {
-
-    println!("{:?}", execute_program("
-        function test(a): number {
-            return a + 1
-        }
-        function main(): number {
-            return test(10)
-        }
-    ", false));
 }
--- packages/web_backend/src/main.rs
+++ packages/web_backend/src/main.rs
@@ -4,18 +4,29 @@ use rocket::{ post, launch, routes, FromForm };
 use rocket::form::Form;
 use rocket::fs::TempFile;
 
-use rocket::serde::{Deserialize, json::Json};
+use rocket::serde::{Deserialize, Serialize, json::Json};
 
 #[derive(Deserialize, Debug)]
 #[serde(crate = "rocket::serde")]
-struct KsCodeRequest<'r> {
-    code: &'r str,
+struct KsCodeRequest {
+    code: String,
     emit_llvm: bool,
+}
+#[derive(Serialize, Debug)]
+#[serde(crate = "rocket::serde")]
+struct KsCodeResponse {
+    llvm_code: String,
+    return_value: f64,
 }
 
 #[post("/run", data = "<code_request>")]
-fn run_ks_code(code_request: Json<KsCodeRequest<'_>>) { 
-    println!("{:?}", code_request);
+fn run_ks_code(code_request: Json<KsCodeRequest>) -> Json<KsCodeResponse> { 
+    let execute_result = execute_program(code_request.code.clone(), code_request.emit_llvm);
+    println!("{:?} || {:?}", code_request, execute_result);
+    match &execute_result.llvm_code {
+        Some(code) => Json(KsCodeResponse{ llvm_code: String::from(code.as_str()), return_value: execute_result.return_value }),
+        None => Json(KsCodeResponse{ llvm_code: String::from(""), return_value: execute_result.return_value })
+    }
  }
 #[launch]
 fn start() -> _ {
